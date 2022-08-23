56602a0d0683d7ff5c5a021d8d8ee8a0ad0518e3
PatrickStar <1592810043@qq.com>
2022-08-23 21:25:42 +0800

    init

---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+require('base')
+require('highlights')
+require('maps')
+require('macos')
+require('plugins')
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,38 @@
+local status, packer = pcall(require, "packer")
+if (not status) then
+  print("Packer is not installed")
+  return
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use { "ellisonleao/gruvbox.nvim" }
+  use 'nvim-lualine/lualine.nvim' -- Statusline
+  use 'nvim-lua/plenary.nvim' -- Common utilities
+  use 'onsails/lspkind-nvim' -- vscode-like pictograms
+  use {
+    'neoclide/coc.nvim', 
+    branch = 'release'
+  }
+  use 'L3MON4D3/LuaSnip'
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+  use 'kyazdani42/nvim-web-devicons' -- File icons
+  use 'nvim-telescope/telescope.nvim'
+  use 'nvim-telescope/telescope-file-browser.nvim'
+  use 'windwp/nvim-autopairs'
+  use 'windwp/nvim-ts-autotag'
+  use 'norcalli/nvim-colorizer.lua'
+  use 'folke/zen-mode.nvim'
+  use({
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
+  })
+  use 'akinsho/nvim-bufferline.lua'
+
+  use 'lewis6991/gitsigns.nvim'
+end)
